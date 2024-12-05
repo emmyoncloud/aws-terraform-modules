dff3d4de00831e76c60b2841cbb374b31210de79
Emmanuel Akor <akormanuel@gmail.com>
2024-12-05 10:40:39 +0100

    Create variables.tf

---
+++ modules/my_module/variables.tf
@@ -0,0 +1,9 @@
+variable "ami" {
+  description = "The AMI to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type"
+  type        = string
+}
